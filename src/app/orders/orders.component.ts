7c6ad77603d857873591e09be8c52f0dcf6b812c
SharkyUgas <sharmarke.ugas@outlook.com>
2019-06-17 13:49:16 +0100

    wired the service to handle get requests to api to populate orders table

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from '../shared/order.service';
 
+
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
@@ -12,7 +13,10 @@ export class OrdersComponent implements OnInit {
   constructor(private service:OrderService) { }
 
   ngOnInit() {
-  this.service.getOrderList().then(res => this.orderList = res);
-  }
+    console.log("yo");
+this.service.getOrderList().then(res => this.orderList = res);
+//console.log("hey");
+
+}
 
 }
